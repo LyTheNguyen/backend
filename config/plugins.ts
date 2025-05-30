00da5d26cbf57cf8e9cf0df6f389f3a6fcec01dd
LyTheNguyenn <your.email@example.com>
2025-05-30 11:51:17 +0700

    edit

--- config/plugins.ts
+++ config/plugins.ts
@@ -16,4 +16,12 @@ export default ({ env }) => ({
         },
       },
     },
+    upload: {
+      config: {
+        provider: 'local',
+        providerOptions: {
+          sizeLimit: 100 * 1024 * 1024, // 100MB
+        },
+      },
+    },
   }); 
